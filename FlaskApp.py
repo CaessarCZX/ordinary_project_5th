18889f4912b73b9a019b2f276179740e06511a17
SamuelCayetano <127262701+SamuelCayetano@users.noreply.github.com>
2023-10-13 10:52:05 -0600

    Add files via upload
    Versión 0.2: Ahora el register, login,, keepSession logout retornan en un jsonify con un mensaje, token, información del usuario. Incluyen conección a la base de datos (solo en el DataBaseConecction.py es necesario rellenar el host, user, password y database con la que se requiera para testear).

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify, session
+import jwt
+from datetime import datetime, timedelta
+from DataBaseConnection import connect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'tu_clave_secreta'  # Cambia esto por una clave segura
+app.config['SECRET_KEY'] = 'ClaveSecreta'
 
 # Ruta de registro
 @app.route('/register', methods=['POST'])
@@ -14,11 +17,57 @@ def registro():
     password = request.form.get('password')
     year = request.form.get('year')
     month = request.form.get('month')
-    day = request.form.get('day')
+    day = request.form.get('day')   
+    # Conectar a la base de datos
+    newConnection = connect()
 
-    # Aquí deberías realizar validaciones de los datos (por ejemplo, si el usuario ya existe)
+    # Crear un cursor para ejecutar consultas
+    cursor = newConnection.cursor()
 
-    # Simplemente almacenaremos los datos en sesión para este ejemplo
+    # Verificar que el correo no existe en la base de datos
+    cursor.execute("SELECT * FROM usuario WHERE correo = %s", (mail,))
+    if cursor.fetchone():
+        cursor.close()
+        newConnection.close()
+        return jsonify({'mensaje': 'El correo ya está registrado'}), 400
+
+    # Verificar que el nombre de usuario no se repita
+    cursor.execute("SELECT * FROM usuario WHERE usuario = %s", (username,))
+    if cursor.fetchone():
+        cursor.close()
+        newConnection.close()
+        return jsonify({'mensaje': 'El nombre de usuario ya está en uso'}), 400
+    
+    # Verificar que la contraseña tenga al menos 6 caracteres
+    if len(password) < 6:
+        cursor.close()
+        newConnection.close()
+        return jsonify({'mensaje': 'La contraseña debe tener al menos 6 caracteres'}), 400
+
+    # Verificar que el usuario sea mayor de 15 años
+    from datetime import date
+    today = date.today()
+    edad = today.year - year - ((today.month, today.day) < (month, day))
+    if edad < 15:
+        cursor.close()
+        newConnection.close()
+        return jsonify({'mensaje': 'Debes tener al menos 15 años para registrarte'}), 400
+
+    # Ejecutar el INSERT INTO en la tabla de usuarios
+    cursor.execute(
+        "INSERT INTO usuario (nombre, apellido, usuario, correo, contraseña, year, month, day) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+        (first_name, last_name, username, mail, password, year, month, day)
+    )
+
+    # Confirmar la transacción
+    newConnection.commit()
+
+    # Cerrar el cursor y la conexión
+    cursor.close()
+    newConnection.close()
+
+
+    # Simplemente almacenaremos los datos en sesión
     user_info = {
         'first_name': first_name, 
         'last_name': last_name, 
@@ -29,38 +78,73 @@ def registro():
         'month': month,
         'day': day
     }
-    session['usuario'] = user_info
+    session['user'] = user_info
 
-    return jsonify({'mensaje': '¡Registro exitoso!', 'user': user_info})
+    token = jwt.encode({'userToken': user_info['username'], 'exp': datetime.utcnow() + timedelta(hours=1)}, app.config['ClaveSecreta'], algorithm='HS256')
+    response = jsonify({'mensaje': '¡Registro exitoso!', 'token': token, 'user': user_info})
+    response.set_cookie('token', token)
+    return response
 
 # Ruta de login
 @app.route('/login', methods=['POST'])
 def login():
     # Obtener datos del formulario de login
-    username = request.form.get('username')
+    mail = request.form.get('username')
     password = request.form.get('password')
+    
+    # Conectar a la base de datos
+    newConnection = connect()
 
-    # Aquí deberías verificar las credenciales (por ejemplo, comparar con la base de datos)
-    # Si las credenciales son válidas, almacenar el usuario en sesión
+    # Crear un cursor para ejecutar consultas
+    cursor = newConnection.cursor(dictionary=True)
 
-    # Para este ejemplo, asumimos que el login es exitoso
-    session['user'] = username
+    # Buscar al usuario por correo y contraseña
+    consulta = "SELECT * FROM mail WHERE password = %s AND contraseña = %s"
+    cursor.execute(consulta, (mail, password))
 
-    return jsonify({'mensaje': '¡Inicio de sesión exitoso!', 'usuario': username})
+    user_info = cursor.fetchone()
+
+    if user_info:
+        # Si las credenciales son válidas, almacenar el usuario en sesión
+        user_info = {
+            'first_name': user_info[0],
+            'last_name': user_info[1],
+            'username': user_info[2],
+            'mail': user_info[3],
+            'password': user_info[4],
+            'year': user_info[5],
+            'month': user_info[6],
+            'day': user_info[7]
+        }
+        session['user'] = user_info
+
+        # Cerrar el cursor y la conexión
+        cursor.close()
+        newConnection.close()
+
+        token = jwt.encode({'userToken': user_info[2], 'exp': datetime.utcnow() + timedelta(hours=1)}, app.config['ClaveSecreta'], algorithm='HS256')
+        response = jsonify({'mensaje': '¡Inicio de sesión exitoso!', 'token': token, 'usuario': user_info})
+        response.set_cookie('token', token)
+        return response
+
+    # Si las credenciales no son válidas
+    cursor.close()
+    newConnection.close()
+    return jsonify({'mensaje': 'Credenciales incorrectas'}), 401
 
 # Ruta para obtener el estado de sesión actual
-@app.route('/estado_sesion', methods=['GET'])
+@app.route('/keep_session', methods=['GET'])
 def estado_sesion():
-    usuario = session.get('usuario')
-    if usuario:
-        return jsonify({'logueado': True, 'usuario': usuario})
+    user_info = session.get('user')
+    if user_info:
+        return jsonify({'logueado': True, 'usuario': user_info})
     else:
         return jsonify({'logueado': False})
 
 # Ruta para cerrar sesión
 @app.route('/logout', methods=['GET'])
 def logout():
-    session.pop('usuario', None)
+    session.pop('user', None)
     return jsonify({'mensaje': '¡Sesión cerrada exitosamente!'})
 
 if __name__ == '__main__':
