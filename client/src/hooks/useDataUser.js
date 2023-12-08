3ba3788bce854c57e99debf18019b7999c698c3a
CesarTun <ctuncetina@gmail.com>
2023-12-08 09:15:09 -0600

    feat: delete client

--- client/src/hooks/useDataUser.js
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux'
-
-export const useDataUser = () => {
-  const userData = useSelector(state => state.auth.user)
-
-  return userData
-}
--- client/src/utils/getValues.js
+++
@@ -1,9 +0,0 @@
-export const isEmptyObject = (userData) => {
-  const isEmpty = Object.values(userData).every(x => x === null || x === '')
-  return isEmpty
-}
-
-export const undefinedToBoolean = (value) => {
-  const valueMiddleware = value !== undefined
-  return valueMiddleware
-}
