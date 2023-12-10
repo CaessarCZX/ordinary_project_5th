430d3153d2daa145f7d3f371c6074c1cb52061ae
CesarTun <ctuncetina@gmail.com>
2023-12-10 16:54:07 -0600

    feat: erase old version client

--- client/src/hooks/useProfile.js
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux'
-
-export const useProfile = () => {
-  const profile = useSelector(state => state.profile)
-
-  return profile
-}
--- client/src/layout/Header/HeaderSearchResultsNotFound.jsx
+++
@@ -1,9 +0,0 @@
-import { SearchResultsWrapper } from '../../styled components/Header-theme'
-
-export function HeaderSearchResultsNotFound () {
-  return (
-    <SearchResultsWrapper>
-      <div>No encontrado papu :V</div>
-    </SearchResultsWrapper>
-  )
-}
